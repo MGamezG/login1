4aaeb985480136b386785b26d0944de2ab13c093
MGamezG <l171130314@gamadero.tecnm.mx>
2022-09-12 12:03:54 -0500

    conect whit model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -16,7 +17,8 @@ import { RegisterComponent } from './pages/register/register.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,3 +1,4 @@
+import { UserModel } from './../../models/user.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  user!:UserModel;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.user=new UserModel();
+    this.user.email='dmfsmdfms@email.com'
+    this.user.name='mg'
+    this.user.password='12432'
+  }
+  OnSubmit(){
+    console.log('formulario enviado');
+    console.log(this.user)
+
   }
 
 }
